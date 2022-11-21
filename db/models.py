7f239cf85c3600851e63a556c05213186079d1a3
Fenomen377 <gkf5153@gmail.com>
2022-11-21 20:21:10 +0300

    upd models and handlers, add new functional

--- db/models.py
+++ db/models.py
@@ -1,4 +1,5 @@
 import psycopg2 as psycopg2
+from aiogram.types import ReplyKeyboardMarkup
 from decouple import config
 
 from create_bot import bot
@@ -112,3 +113,92 @@ def psql_read():
     query_results = cur.fetchall()
     text = '🧾Категории меню:' + '\n—' + '\n—'.join([', '.join(map(str, x)) for x in query_results])
     return str(text)
+
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS Типы (
+    id SERIAL PRIMARY KEY,
+    Тип TEXT);
+
+''')
+con.commit()
+
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS Столики (
+    id SERIAL PRIMARY KEY,
+    Столик INTEGER NOT NULL,
+    Тип INT NOT NULL,
+    FOREIGN KEY(Тип) REFERENCES Типы,
+    place INTEGER NOT NULL);
+
+''')
+con.commit()
+
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS Бронирование (
+    id SERIAL PRIMARY KEY,
+    Столик INTEGER,
+    Дата DATE NOT NULL,
+    FOREIGN KEY(Столик) REFERENCES Столики(id),
+    CONSTRAINT Бронь UNIQUE (Столик, Дата));
+''')
+con.commit()
+
+
+def table_read():
+    cur.execute('SELECT * FROM Типы')
+    query_results = cur.fetchall()
+    text = 'В каком зале вы хотите забронировать стол?'
+    return str(text)
+
+# 🗓На какое число желаете забронировать стол?\nВведите дату в формате YYYY-MM-DD
+
+
+def table_VIP():
+    cur.execute('SELECT Столик, place FROM Столики WHERE Тип=1')
+    query_results = cur.fetchall()
+    for row in query_results:
+        text = '⚜VIP-зал⚜\n\n🛑Выберите стол для бронирования🛑\n'+'\n'.join([f''.join(map(str, '\n' + 'VIP-зал №' + str(x[0]) + ' | ' + 'Количество мест: ' + str(x[1]))) for x in query_results])
+        return str(text)
+
+
+def table_mer():
+    cur.execute('SELECT Столик, place FROM Столики WHERE Тип=2')
+    query_results = cur.fetchall()
+    for row in query_results:
+        text = 'Зал мероприятий\n\n🛑Выберите стол для бронирования🛑\n'+'\n'.join([f''.join(map(str, '\n' + 'Столик №' + str(x[0]) + ' | ' + 'Количество мест: ' + str(x[1]))) for x in query_results])
+        return str(text)
+
+
+def table_ver():
+    cur.execute('SELECT Столик, place FROM Столики WHERE Тип=3')
+    query_results = cur.fetchall()
+    for row in query_results:
+        text = 'Веранда\n\n🛑Выберите стол для бронирования🛑\n'+'\n'.join([f''.join(map(str, '\n' + 'Столик №' + str(x[0]) + ' | ' + 'Количество мест: ' + str(x[1]))) for x in query_results])
+        return str(text)
+
+
+def table_1():
+    cur.execute('SELECT Столик, place FROM Столики WHERE Тип=4')
+    query_results = cur.fetchall()
+    for row in query_results:
+        text = 'Зал №1\n\n🛑Выберите стол для бронирования🛑\n'+'\n'.join([f''.join(map(str, '\n' + 'Столик №' + str(x[0]) + ' | ' + 'Количество мест: ' + str(x[1]))) for x in query_results])
+        return str(text)
+
+
+def table_2():
+    cur.execute('SELECT Столик, place FROM Столики WHERE Тип=5')
+    query_results = cur.fetchall()
+    for row in query_results:
+        text = 'Зал №2\n\n🛑Выберите стол для бронирования🛑\n'+'\n'.join([f''.join(map(str, '\n' + 'Столик №' + str(x[0]) + ' | ' + 'Количество мест: ' + str(x[1]))) for x in query_results])
+        return str(text)
+
+
+def table_3():
+    cur.execute('SELECT Столик, place FROM Столики WHERE Тип=6')
+    query_results = cur.fetchall()
+    for row in query_results:
+        text = 'Зал №3\n\n🛑Выберите стол для бронирования🛑\n'+'\n'.join([f''.join(map(str, '\n' + 'Столик №' + str(x[0]) + ' | ' + 'Количество мест: ' + str(x[1]))) for x in query_results])
+        return str(text)
--- handlers/client.py
+++ handlers/client.py
@@ -29,6 +29,17 @@ async def send_welcome(message: types.Message):
 
 # @dp.message_handler()
 async def commands(message: types.Message):
+    kb = [
+        [
+            types.KeyboardButton(text="Забронировать стол"),
+            types.KeyboardButton(text="Меню"),
+            types.KeyboardButton(text="Контакты"),
+            types.KeyboardButton(text='Режим работы'),
+            types.KeyboardButton(text='Расположение'),
+
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True).add(urlButton)
     kbs =[
         [
             types.KeyboardButton(text='Блюда из печи'),
@@ -44,8 +55,42 @@ async def commands(message: types.Message):
     but6 = KeyboardButton(text='Десерты')
     but7 = KeyboardButton(text='Блюда на мангале')
     but8 = KeyboardButton(text='Закуски')
-    but9 = KeyboardButton(text='/start')
-    keyboard = types.ReplyKeyboardMarkup(keyboard=kbs, resize_keyboard=True).add(but7, but8, but1, but2, but3, but4, but5, but6, but9)
+    but9 = KeyboardButton(text='Вернуться в главное меню')
+    keyboard1 = types.ReplyKeyboardMarkup(keyboard=kbs, resize_keyboard=True).add(but7, but8, but1, but2, but3, but4, but5, but6, but9)
+    kbt = [
+        [
+            types.KeyboardButton(text='VIP'),
+            types.KeyboardButton(text='Зал мероприятий')
+        ],
+    ]
+    butt3 = KeyboardButton(text='Веранда')
+    butt4 = KeyboardButton(text='Зал №1')
+    butt5 = KeyboardButton(text='Зал №2')
+    butt6 = KeyboardButton(text='Зал №3')
+    keyboard2 = types.ReplyKeyboardMarkup(keyboard=kbt, resize_keyboard=True).add(butt3, butt4).add(butt5, butt6).add(but9)
+    kba = [
+        [
+            types.KeyboardButton(text='Столик №1'),
+            types.KeyboardButton(text='Столик №2'),
+        ],
+    ]
+    kp1 = KeyboardButton(text='Столик №3')
+    kp2 = KeyboardButton(text='Столик №4')
+    kp3 = KeyboardButton(text='Столик №5')
+    kp4 = KeyboardButton(text='Столик №6')
+    kp5 = KeyboardButton(text='Столик №7')
+    kp6 = KeyboardButton(text='Столик №8')
+    kp7 = KeyboardButton(text='Столик №9')
+    kp8 = KeyboardButton(text='Столик №10')
+    kp9 = KeyboardButton(text='Назад')
+    kbb = [
+        [
+            types.KeyboardButton(text='VIP-зал №1'),
+            types.KeyboardButton(text='VIP-зал №2')
+        ],
+    ]
+    keyboard3 = types.ReplyKeyboardMarkup(keyboard=kba, resize_keyboard=True).add(kp1, kp2).add(kp3, kp4).add(kp5, kp6).add(kp7, kp8).add(kp9)
+    keyboard4 = types.ReplyKeyboardMarkup(keyboard=kbb, resize_keyboard=True).add(kp9)
     if message.text == 'Контакты':
         await message.reply('Администратор: @kgf_kamil')
     elif message.text == 'Режим работы':
@@ -54,12 +99,12 @@ async def commands(message: types.Message):
         await message.reply('г. Москва, ул. Пушкина, д. Колотушкина 66')
     elif message.text == 'Перейти на сайт ресторана🇬🇪':
         await message.reply('Вы можете перейти на сайт ресторана по ссылке: gamarjoba.sytes.net')
-    elif message.text == 'Забронировать стол':
-        await message.reply('🗓Когда планируете посетить наш ресторан?\nВведите дату в формате YYYY-MM-DD')
     elif message.text == 'Меню':
-        await message.reply(psql_read(), reply_markup=keyboard)
+        await message.reply(psql_read(), reply_markup=keyboard1)
     elif message.text == 'Блюда из печи':
         await message.reply(psql_read_pechka())
+    elif message.text == 'Забронировать стол':
+        await message.reply(table_read(), reply_markup=keyboard2)
     elif message.text == 'Салаты':
         await message.reply(psql_read_salati())
     elif message.text == 'Горячие блюда':
@@ -80,6 +125,22 @@ async def commands(message: types.Message):
         await message.reply(psql_read_napitki())
     elif message.text == 'Десерты':
         await message.reply(psql_read_desert())
+    elif message.text == 'VIP':
+        await message.reply(table_VIP(), reply_markup=keyboard4)
+    elif message.text == 'Зал мероприятий':
+        await message.reply(table_mer(), reply_markup=keyboard3)
+    elif message.text == 'Веранда':
+        await message.reply(table_ver(), reply_markup=keyboard3)
+    elif message.text == 'Зал №1':
+        await message.reply(table_1(), reply_markup=keyboard3)
+    elif message.text == 'Зал №2':
+        await message.reply(table_2(), reply_markup=keyboard3)
+    elif message.text == 'Зал №3':
+        await message.reply(table_3(), reply_markup=keyboard3)
+    elif message.text == 'Назад':
+        await message.reply('Вы вернулись назад', reply_markup=keyboard2)
+    elif message.text == 'Вернуться в главное меню':
+        await message.reply('Вы вернулись в главное меню', reply_markup=keyboard)
 
 
 def register_handlers_client(dp : Dispatcher):
